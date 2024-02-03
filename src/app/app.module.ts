9bfdc53656a350f76bcd68711368b7bfb95557eb
JOHAN OLARTE <johandres7@hotmail.com>
2024-02-03 10:40:28 -0500

    Preparacion inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { GalleriaModule } from 'primeng/galleria';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
